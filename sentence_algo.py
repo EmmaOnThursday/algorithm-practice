51932ca71b963368971cfd60f31ff960560b5469
Emma Ferguson <emmamferguson@gmail.com>
2016-01-29 20:48:27 -0800

    fixed weird weird extra space as first character issue; updated doc string

--- sentence_algo.py
+++ sentence_algo.py
@@ -10,11 +10,14 @@ def sentence_reverse(any_string):
     """Given a string with any number of spaces and words, returns the sentence reversed.
 
     >>> sentence_reverse("policemen wear   blue  especially on  Thursdays.")
-    ' Thursdays.  on especially  blue   wearpolicemen'
+    'Thursdays.  on especially  blue   wear policemen'
     """
-    diff_sentence = sentence.replace(" ", "# ")
+    diff_sentence = sentence.replace(" ", "# #")
     sent_list = diff_sentence.split("#")
     sent_list.reverse()
 
     reversed_sentence = "".join(sent_list)
     return reversed_sentence
+
+
+print sentence_reverse(sentence)
