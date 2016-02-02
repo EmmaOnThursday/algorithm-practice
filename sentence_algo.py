c61c465593a59ef297f6cbf25999f85985b218f6
Emma Ferguson <emmamferguson@gmail.com>
2016-02-01 22:00:26 -0800

    simplified algorithm solution

--- sentence_algo.py
+++ sentence_algo.py
@@ -1,10 +1,5 @@
 sentence = "policemen wear   blue  especially on  Thursdays."
 
-# replace spaces with another character + space
-# split on other character & append all to list
-# reverse list
-# join reversed list
-
 
 def sentence_reverse(any_string):
     """Given a string with any number of spaces and words, returns the sentence reversed.
@@ -12,11 +7,11 @@ def sentence_reverse(any_string):
     >>> sentence_reverse("policemen wear   blue  especially on  Thursdays.")
     'Thursdays.  on especially  blue   wear policemen'
     """
-    diff_sentence = sentence.replace(" ", "# #")
-    sent_list = diff_sentence.split("#")
+
+    sent_list = diff_sentence.split(" ")
     sent_list.reverse()
 
-    reversed_sentence = "".join(sent_list)
+    reversed_sentence = " ".join(sent_list)
     return reversed_sentence
 
 
