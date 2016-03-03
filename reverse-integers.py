50f589ef8410c1257eaab7a577240dad2eeb12b5
Emma Ferguson <emmamferguson@gmail.com>
2016-03-03 15:53:52 -0800

    adding algorithm practice docs

---
+++ reverse-integers.py
@@ -0,0 +1,18 @@
+
+
+"""Given a string of n integers, return the string reversed.
+Do not "listify" the string at any point. 
+
+Input: 012345
+Output: 543210
+
+"""
+
+index_dict = {}
+
+for x in string:
+    string.index(x) = i
+    index_dict.get(i, x)
+
+
+
